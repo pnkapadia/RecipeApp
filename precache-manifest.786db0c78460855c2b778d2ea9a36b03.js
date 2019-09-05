611146f4e6a50326f1840b8f18cec6157d2cebe8
prachikapadia <prachi.kapadia@hotmail.com>
2019-09-04 23:33:26 -0400

    Updates

---
+++ precache-manifest.786db0c78460855c2b778d2ea9a36b03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12c637485ffa62f7a6b3000f8675f33",
+    "url": "/recipeApp/index.html"
+  },
+  {
+    "revision": "c2c04c1bf66838c2ed5c",
+    "url": "/recipeApp/static/css/main.ecb9e686.chunk.css"
+  },
+  {
+    "revision": "3871d5f66d4d0197bc6d",
+    "url": "/recipeApp/static/js/2.67a72fef.chunk.js"
+  },
+  {
+    "revision": "c2c04c1bf66838c2ed5c",
+    "url": "/recipeApp/static/js/main.48f5bb7c.chunk.js"
+  },
+  {
+    "revision": "105dd21ae6d841f3da80",
+    "url": "/recipeApp/static/js/runtime~main.a807b1e9.js"
+  }
+]);
